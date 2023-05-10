9f23bbce9e6dfafafddde9df50c63ef987ba624d
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:43:08 -0300

    Update Ex12.java

--- Ex12.java
+++ Ex12.java
@@ -4,10 +4,10 @@ public class Ex12 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe a sua altura: ");
-        float h = sc.nextFloat();
-        float pi1 = (float) 72.7*h-58;
+        double h = sc.nextDouble();
+        sc.close();
+        double pi1 = (72.7*h)-58;
         System.out.printf("Seu peso ideal é: %.2f\n", pi1);
-       
     }
 }
     
