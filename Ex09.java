d64504d70936afb4379493ea2df0fe880231f6a4
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:39:10 -0300

    Update Ex09.java

--- Ex09.java
+++ Ex09.java
@@ -4,8 +4,9 @@ public class Ex09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe a temperatura em Fahrenheit: ");
-        float F = sc.nextFloat();
-        float C = 5*((F-32)/9);
+        double F = sc.nextDouble();
+        sc.close();
+        double C = 5*((F-32)/9);
         System.out.printf("A temperatura em Celsius é: %.2f\n", C );
     }
 }
