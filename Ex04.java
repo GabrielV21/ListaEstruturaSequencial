b9050775f5dd70f1acc22d846704605e9fd4e98e
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:56:17 -0300

    Update Ex04.java

--- Ex04.java
+++ Ex04.java
@@ -2,17 +2,14 @@ package listadeexercicio;
 import java.util.Scanner;
 public class Ex04 {
     public static void main(String[] args) {
-        
         Scanner sc = new Scanner(System.in);
         int i;
-        float nota[] = new float[4];
+        double nota[] = new double[4];
         for (i=0; i<4; i++){
             System.out.println("Informe a nota: ");
-            nota[i] = sc.nextFloat();
+            nota[i] = sc.nextDouble();
           }
         sc.close();
         System.out.println("A média das notas é: " + ((nota[0]+nota[1]+nota[2]+nota[3])/4) );
-        
     } 
-    
 }
