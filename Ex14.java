ca9fc6bbac5a8473de7f4a2f1bef8066af13752b
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:46:07 -0300

    Update Ex14.java

--- Ex14.java
+++ Ex14.java
@@ -5,11 +5,11 @@ public class Ex14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe o peso da pesca: ");
-        float pesopesca = sc.nextFloat();
-        
+        double pesopesca = sc.nextDouble();
+        sc.close();
         if (pesopesca>50){
-            float excesso = pesopesca-50;
-            float multa = excesso*4;
+            double excesso = pesopesca-50;
+            double multa = excesso*4;
             System.out.println("O peso em excesso é de: " + excesso);
             System.out.println("E o valor da multa é de: " + multa);
         } else {
