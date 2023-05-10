8831963dcb3fedcd007302d42768b405e3a9ab3b
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:40:13 -0300

    Update Ex10.java

--- Ex10.java
+++ Ex10.java
@@ -4,8 +4,8 @@ public class Ex10 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe a temperatura em Celsius: ");
-        float C = sc.nextFloat();
-        float F = (float)(C*1.8)+32;
+        double C = sc.nextDouble();
+        double F = (C*1.8)+32;
         System.out.printf("A temperatura em Fahrenheit é: %.2f\n", F );
     }
 }
