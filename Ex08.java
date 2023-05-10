4ff0d4eab9c1afc6cb4a91e12aaf1c0c1136b42c
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:28:43 -0300

    Update Ex08.java

--- Ex08.java
+++ Ex08.java
@@ -4,9 +4,9 @@ public class Ex08 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe quanto você ganha por hora: ");
-        float salario = sc.nextFloat();
+        double salario = sc.nextDouble();
         System.out.println("Informe quantas horas você trabalha por mês; ");
-        float horas = sc.nextFloat();
+        double horas = sc.nextDouble();
         System.out.println("Seu salário é: R$" + salario * horas);
     }
 }
