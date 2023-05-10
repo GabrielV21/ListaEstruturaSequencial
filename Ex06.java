cffb399889c2891f417edc166b29ee0066797422
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:24:59 -0300

    Update Ex06.java

--- Ex06.java
+++ Ex06.java
@@ -4,8 +4,9 @@ public class Ex06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Informe o raio do circulo: ");
-        float raio = sc.nextFloat();
-        float area = (float)(3.14*(raio*raio)); 
+        double raio = sc.nextDouble();
+        sc.close();
+        double area = (3.14*(raio*raio)); 
         System.out.println("A area do circulo é: " + area);
     }
 }
