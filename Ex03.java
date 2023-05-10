c4ebe9b55c57f01dd37e14b079aca0dd5d29230e
GabrielVasconcelos <128516900+GabrielV21@users.noreply.github.com>
2023-05-09 23:18:12 -0300

    Update Ex03.java

--- Ex03.java
+++ Ex03.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class Ex03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Short num1, num2;
+        double num1, num2;
         System.out.println("Informe o primeiro numero: ");
-        num1 = sc.nextShort();
+        num1 = sc.nextDouble();
         System.out.println("Informe o segundo numero: ");
-        num2 = sc.nextShort();
+        num2 = sc.nextDouble();
         System.out.println("A soma dos dois numeros informados foi: " + (num1+num2));
     }
 }
